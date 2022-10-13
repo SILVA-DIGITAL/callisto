d9f532b84acd8c2b849a6bca448090c64b19c720
Maximillian <maxsilvauk@gmail.com>
2022-10-13 01:15:40 +0100

    Stars added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,34 @@
+import { Suspense } from 'react'
+import { Html, useProgress, useGLTF } from '@react-three/drei'
+import { Stars } from '@/components/stars'
+
 const Page = (props) => {
   return <></>
 }
 
-Page.r3f = (props) => <></>
+// function Helmet() {
+//   const { nodes } = useGLTF(
+//     'Straw.gltf'
+//   )
+//   return <primitive object={nodes['node_damagedHelmet_-6514']} />
+// }
+
+function CustomLoader() {
+  const { progress } = useProgress()
+  return (
+    <>
+      <Html style={{ color: 'white' }}>{progress} % loaded</Html>
+    </>
+  )
+}
+
+Page.r3f = (props) => (
+  <>
+    <Suspense fallback={<CustomLoader />}>
+      <Stars />
+    </Suspense>
+  </>
+)
 
 export default Page
 
