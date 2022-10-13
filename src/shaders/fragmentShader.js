25fa3c983037d85f089a61b8afe382d3e02822b0
Maximillian <maxsilvauk@gmail.com>
2022-10-13 14:32:08 +0100

    Updated fragmentShader to show white FragColor

--- src/shaders/fragmentShader.js
+++ src/shaders/fragmentShader.js
@@ -2,6 +2,6 @@ export const fragmentShader = `
     void main() {
       float alpha = 1. - smoothstep(0.2995, 0.5005, length(gl_PointCoord - vec2(0.5)));
 
-      gl_FragColor = vec4(vec3(0.0), alpha);
+      gl_FragColor = vec4(vec3(1.0,2.0,1.0),0.2);
     }
   `
