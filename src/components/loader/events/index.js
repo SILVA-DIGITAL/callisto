2734f63d107d974ac681b887278101eca73c5deb
Maximillian <maxsilvauk@gmail.com>
2022-10-14 12:04:50 +0100

    Removed FBOLoader comoponent

--- src/components/loader/events/index.js
+++
@@ -1,5 +0,0 @@
-import Events from './Emitter'
-import GlobalRaf from './Raf'
-import GlobalResize from './Resize'
-
-export { Events, GlobalRaf, GlobalResize }
--- src/components/loader/gui.js
+++
@@ -1,6 +0,0 @@
-import * as dat from 'dat.gui'
-
-const gui = new dat.GUI()
-gui.width = 300
-
-export default gui
