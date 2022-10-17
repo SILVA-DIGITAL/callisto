1c912464abd95d5b6674c1024c708277e895f374
Maximillian <maxsilvauk@gmail.com>
2022-10-17 02:41:31 +0100

    Update statue.jsx

--- src/pages/statue.jsx
+++ src/pages/statue.jsx
@@ -1,28 +1,16 @@
-import { Suspense } from 'react'
-import { Environment, Html, useProgress } from '@react-three/drei'
+import { Environment } from '@react-three/drei'
 import { Statue } from '@/components/statue'
 import { Lights } from '@/components/lights'
 
-function CustomLoader() {
-  const { progress } = useProgress()
-  return (
-    <>
-      <Html style={{ color: 'white' }}>{progress} % loaded</Html>
-    </>
-  )
-}
-
-const StatuePage = (props) => {
+const StatuePage = () => {
   return <></>
 }
 
-StatuePage.r3f = (props) => (
+StatuePage.r3f = () => (
   <>
-    <Suspense fallback={<CustomLoader />}>
-      <Lights />
-      <Statue />
-      <Environment background preset='night' />
-    </Suspense>
+    <Lights />
+    <Statue />
+    <Environment background preset='night' />
   </>
 )
 
