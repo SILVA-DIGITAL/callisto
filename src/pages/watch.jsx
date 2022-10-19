aae040345c243d2d08d3c96799711b334f9b3d7a
Maximillian <maxsilvauk@gmail.com>
2022-10-19 02:23:05 +0100

    Update watch.jsx

--- src/pages/watch.jsx
+++ src/pages/watch.jsx
@@ -6,34 +6,32 @@ import {
 import { Watch } from '@/components/watch'
 
 const WatchPage = (props) => {
-  return <></>
-}
-
-WatchPage.r3f = (props) => (
-  <>
-    <PresentationControls
-      global
-      config={{ mass: 2, tension: 500 }}
-      rotation={[0, 0.3, 0]}
-      polar={[-Math.PI / 3, Math.PI / 3]}
-      azimuth={[-Math.PI / 1.4, Math.PI / 2]}
-    >
-      <Watch
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, 0.25, 0]}
-        scale={0.003}
+  return (
+    <>
+      <PresentationControls
+        global
+        config={{ mass: 2, tension: 500 }}
+        rotation={[0, 0.3, 0]}
+        polar={[-Math.PI / 3, Math.PI / 3]}
+        azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+      >
+        <Watch
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, 0.25, 0]}
+          scale={0.003}
+        />
+      </PresentationControls>
+      <ContactShadows
+        position={[0, -1.4, 0]}
+        opacity={0.75}
+        scale={10}
+        blur={2.5}
+        far={4}
       />
-    </PresentationControls>
-    <ContactShadows
-      position={[0, -1.4, 0]}
-      opacity={0.75}
-      scale={10}
-      blur={2.5}
-      far={4}
-    />
-    <Environment preset='city' />
-  </>
-)
+      <Environment preset='city' />
+    </>
+  )
+}
 
 export default WatchPage
 
