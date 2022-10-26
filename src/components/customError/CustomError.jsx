0b189c39cb6949a2f848497fb8d9bfa85e2105a5
Maximillian <maxsilvauk@gmail.com>
2022-10-26 21:10:29 +0100

    Fixing the tailwind warnings

--- src/components/customError/CustomError.jsx
+++ src/components/customError/CustomError.jsx
@@ -5,7 +5,7 @@ export const CustomError = ({ type }) => {
       : `Please try again later an error has occurred`
   return (
     <div
-      className='absolute max-w-lg px-4 py-2 text-sm bg-gray-900 shadow-xl pointer-events-none select-none md:text-base top-8 left-1/2 text-gray-50 transform -translate-x-1/2'
+      className='absolute max-w-lg px-4 py-2 text-sm bg-gray-900 shadow-xl pointer-events-none select-none md:text-base top-8 left-1/2 text-gray-50 -translate-x-1/2'
       style={{
         width: '520px',
         maxWidth: 'calc(100% - 28px)',
