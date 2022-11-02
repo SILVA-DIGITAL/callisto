647f6455f3f5fee1ec6c1bee5102fa98b4c3f642
Maximillian <maxsilvauk@gmail.com>
2022-11-02 00:00:24 +0000

    Update 500.jsx

--- src/pages/500.jsx
+++ src/pages/500.jsx
@@ -3,7 +3,7 @@ import { CustomError } from '@/components/customError/CustomError'
 export default function Error() {
   return (
     <>
-      <CustomError type='500' />
+      <CustomError type='500wdwd' />
     </>
   )
 }
