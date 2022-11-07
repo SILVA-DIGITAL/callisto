6369126224cdefc33128707b0a2286a6d93ae774
Maximillian <maxsilvauk@gmail.com>
2022-11-07 15:48:53 +0000

    First attempt to get data from hygraph using graphql-request

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,6 @@
+// import { gql } from '@apollo/client'
+// import client from '@/lib/apolloClient'
+import { GraphQLClient } from 'graphql-request'
 import { Stars } from '@/components/stars'
 
 const HomePageDOM = () => {
@@ -10,7 +13,8 @@ const HomePageR3F = () => (
   </>
 )
 
-export default function HomePage() {
+export default function HomePage({ demoModels }) {
+  console.log('demoModels', demoModels)
   return (
     <>
       <HomePageDOM />
@@ -20,9 +24,29 @@ export default function HomePage() {
 }
 
 export async function getStaticProps() {
+  const hygraph = new GraphQLClient(
+    'https://api-eu-west-2.hygraph.com/v2/cla5fmzsx46vo01up5txp92w5/master'
+  )
+
+  const { demoModels } = await hygraph.request(
+    `
+      {
+        demoModels {
+          createdAt
+          id
+          publishedAt
+          slug
+          subtitle
+          title
+          updatedAt
+        }
+      }
+    `
+  )
+
   return {
     props: {
-      title: 'Home',
+      demoModels,
     },
   }
 }
