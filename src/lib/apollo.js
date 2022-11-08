6e95159c60cc28235c6165f7c8f1bd5dc5882aa6
Maximillian <maxsilvauk@gmail.com>
2022-11-08 02:26:40 +0000

    Added InMemoryCache to apollo client

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -3,6 +3,7 @@ import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
   uri: process.env.HYGRAPH_URI,
+  cache: new InMemoryCache(),
 })
 
 const authLink = setContext((_, { headers }) => {
