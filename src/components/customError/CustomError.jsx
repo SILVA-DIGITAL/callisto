39a737d06c4e23b666aafd25677f7be88775a94d
Maximillian <maxsilvauk@gmail.com>
2022-11-09 15:16:19 +0000

    Update CustomError.jsx

--- src/components/customError/CustomError.jsx
+++ src/components/customError/CustomError.jsx
@@ -4,19 +4,13 @@ export const CustomError = ({ type }) => {
       ? `The page could not be located`
       : `Please try again later an error has occurred`
   return (
-    <div
-      className='absolute max-w-lg px-4 py-2 text-sm bg-gray-900 shadow-xl pointer-events-none select-none md:text-base top-8 left-1/2 text-gray-50 -translate-x-1/2'
-      style={{
-        width: '520px',
-        maxWidth: 'calc(100% - 28px)',
-      }}
-    >
-      <h1 className='mb-5 text-2xl text-center'>{type}</h1>
-      <div className='tracking-wider'>
-        <p className='text-lg text-center text-red-200'>
+    <>
+      <div class='grid h-screen place-items-center'>
+        <div className='text-center tracking-wider'>
+          <h1 className='px-6 py-4 text-center text-3xl'>{type} &#128565;</h1>
           Something went wrong. {msg}.
-        </p>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
