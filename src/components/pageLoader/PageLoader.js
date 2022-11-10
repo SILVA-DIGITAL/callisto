ddd7efa7abc5ef6961dedfc6e8df75c44dad2e39
Maximillian <maxsilvauk@gmail.com>
2022-11-10 19:07:33 +0000

    Update PageLoader.js

--- src/components/pageLoader/PageLoader.js
+++ src/components/pageLoader/PageLoader.js
@@ -1,11 +1,14 @@
 import { SphereParticles } from '@/components/particles/SphereParticles'
-import { OrthographicCamera } from '@react-three/drei'
+import { OrthographicCamera, Html, useProgress } from '@react-three/drei'
 
 const PageLoader = ({ zoom }) => {
+  const { active, progress, errors, item, loaded, total } = useProgress()
+
   return (
     <>
       <OrthographicCamera makeDefault position={[0, 0, 1]} zoom={zoom} />
       <SphereParticles />
+      <Html center>{progress} % loaded</Html>
     </>
   )
 }
