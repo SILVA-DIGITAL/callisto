136388e4ad28848191d1f917bd9a1c613e5cfe15
Maximillian <maxsilvauk@gmail.com>
2022-11-18 14:43:50 +0000

    Fix showPerf

--- src/utilities/showPerf.jsx
+++ src/utilities/showPerf.jsx
@@ -3,7 +3,7 @@ import { Perf } from 'r3f-perf'
 
 const showPerf = () => {
   if (process.env.NODE_ENV === env.DEVELOPMENT) {
-    return <Perf position='bottom-left' />wdwd
+    return <Perf position='bottom-left' />
   }
 }
 
